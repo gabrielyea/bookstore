54c4982413da8282f26f8909b01e892fad805f19
Gabriel <gabriel.gabrielyea@gmail.com>
2021-08-23 12:29:22 -0500

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { add } from './redux/books/books';
+import {
+  Route,
+  Switch,
+  useLocation,
+} from 'react-router-dom';
+import NavBar from './components/nav/NavBar';
+import Home from './pages/home/Home';
+import Categories from './pages/categories/Categories';
+import NotMatch from './pages/NotMatch';
 
 function App() {
-  const books = useSelector((state) => state.counter.value);
-  const dispatch = useDispatch();
+  const location = useLocation();
   return (
     <div className="App">
-      hello
-      <div>{books}</div>
-      <button type="button" aria-label="add" onClick={() => dispatch(add())}>
-        click here
-      </button>
+      <NavBar />
+      <Switch location={location} key={location.pathname}>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/categories">
+          <Categories />
+        </Route>
+        <Route path="*">
+          <NotMatch />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import store from './redux/configureStore';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router basename={process.env.PUBLIC_URL}>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
