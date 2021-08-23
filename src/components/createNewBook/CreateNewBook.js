4250a4f0206d292c8375f93d0bc291cea54ea11c
Gabriel <gabriel.gabrielyea@gmail.com>
2021-08-23 16:02:00 -0500

    Update CreateNewBook

--- src/components/createNewBook/CreateNewBook.js
+++ src/components/createNewBook/CreateNewBook.js
@@ -1,9 +1,34 @@
-const CreateNewBook = () => (
-  <form>
-    <input label="Title" type="text" placeholder="Book title" />
-    <input label="Author" type="text" placeholder="Book author" />
-    <input label="Create" type="submit" name="Create" value="Create" />
-  </form>
-);
+/* eslint-disable no-unused-vars */
+import { useDispatch } from 'react-redux';
+import { useRef } from 'react';
+import { addBook } from '../../redux/books/books';
+
+const CreateNewBook = () => {
+  const dispatch = useDispatch();
+  const titleInput = useRef(null);
+  const authorInput = useRef(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(titleInput?.current?.value);
+    console.log(authorInput?.current?.value);
+  };
+
+  // const submitBookToStore = () => {
+  //   const newBook = {
+  //     id: 'id',
+  //     title: 'title',
+  //     author: 'author',
+  //   };
+  // };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input ref={titleInput} type="text" name="title" placeholder="Book title" />
+      <input ref={authorInput} type="text" name="author" placeholder="Book author" />
+      <input label="Create" type="submit" name="create" value="Create" />
+    </form>
+  );
+};
 
 export default CreateNewBook;
