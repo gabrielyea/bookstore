1d2ea3e36cabfd80e5394f2dcc929adadb2f9182
Gabriel <gabriel.gabrielyea@gmail.com>
2021-08-23 12:28:55 -0500

    Add navbar component

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,95 +1,36 @@
-import { motion } from 'framer-motion';
 import React from 'react';
-import Media from 'react-media';
-import { NavLink } from 'react-router-dom';
-import styles from './navBar.module.scss';
-import MobileMenu from '../mobile-menu/MobileMenu';
+import MenuItem from './menuItem/MenuItem';
 
 const NavBar = () => {
   const links = [
     {
       id: 1,
       path: '/',
-      text: 'Home',
+      text: 'Books',
     },
     {
       id: 2,
-      path: '/calculator',
-      text: 'Calculator',
-    },
-    {
-      id: 3,
-      path: '/quote',
-      text: 'Joke',
+      path: '/categories',
+      text: 'Categories',
     },
   ];
 
-  const variants = {
-    open: {
-      transition: { staggerChildren: 0.07, delayChildren: 0.2 },
-    },
-    closed: {
-      transition: { staggerChildren: 0.05, staggerDirection: -1 },
-    },
-  };
-
   const createLinks = (linkCollection) => linkCollection.map((link) => (
-    <motion.li
+    <MenuItem
       key={link.id}
-    >
-
-      <NavLink
-        to={link.path}
-        className={styles.link}
-        activeClassName={styles.active}
-        exact
-      >
-        <motion.button
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-        >
-
-          {link.text}
-        </motion.button>
-      </NavLink>
-    </motion.li>
+      index={link.id}
+      to={link.path}
+      text={link.text}
+    />
   ));
 
   return (
-    <>
-      <Media queries={{
-        small: '(max-width: 599px)',
-        medium: '(min-width: 600px) and (max-width: 1980px)',
-        large: '(min-width: 1200px)',
-      }}
-      >
-        {(matches) => (
-          <>
-            {matches.small
-            && (
-            <nav className={styles.navBar}>
-              <MobileMenu
-                variants={variants}
-                createLinksFunc={createLinks}
-                links={links}
-              />
-            </nav>
-            )}
-            {matches.medium
-            && (
-            <nav className={styles.navBar}>
-              <div className="logo"><h1>Math Magicians</h1></div>
-              <motion.ul
-                variants={variants}
-              >
-                {createLinks(links)}
-              </motion.ul>
-            </nav>
-            )}
-          </>
-        )}
-      </Media>
-    </>
+    <nav className="nav">
+      <div className="logo"><h1>Bookstore</h1></div>
+      <ul>
+        {createLinks(links)}
+      </ul>
+    </nav>
   );
 };
 
