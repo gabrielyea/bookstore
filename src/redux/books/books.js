7abe4b83d1236ff12ccb73c9cdb79d63308fdd93
Gabriel <gabriel.gabrielyea@gmail.com>
2021-08-23 12:27:48 -0500

    Add add and remove actions to book reducer

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -4,14 +4,17 @@ import { createSlice } from '@reduxjs/toolkit';
 export const bookSlice = createSlice(
   {
     name: 'counter',
-    initialState: { value: [''] },
+    initialState: { value: [] },
     reducers: {
       add: (state) => {
-        state.value = [...state.value, ' newbook'];
+        state.value = [...state.value].concat(' new book');
+      },
+      remove: (state) => {
+        state.value = [...state.value].splice(0, 0);
       },
     },
   },
 );
 
-export const { add } = bookSlice.actions;
+export const { add, remove } = bookSlice.actions;
 export default bookSlice.reducer;
