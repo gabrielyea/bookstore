ae64cd204cffff54114a0a030c172492625aab42
Gabriel <gabriel.gabrielyea@gmail.com>
2021-08-23 16:54:02 -0500

    Add remove event to book

--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -1,19 +1,28 @@
 /* eslint-disable react/prop-types */
+import { useDispatch } from 'react-redux';
 import React from 'react';
+import { removeBook } from '../../redux/books/books';
 
 const Book = (props) => {
-  const { title, author } = props;
+  const dispatch = useDispatch();
+  const { id, title, author } = props;
+  const handleRemove = () => {
+    dispatch(removeBook(id));
+  };
+
   return (
     <div>
       <p>
         I am a book, my title is
+        {' '}
         {title}
       </p>
       <p>
         And my author is
+        {' '}
         {author}
       </p>
-      <button type="button">Remove me</button>
+      <button onClick={handleRemove} type="button">Remove me</button>
     </div>
   );
 };
