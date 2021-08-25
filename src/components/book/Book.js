02206cf5204d59641df8bf09fde20ae64cba99f3
Gabriel <gabriel.gabrielyea@gmail.com>
2021-08-25 14:35:58 -0500

    Add style for book component

--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux';
 import { React, useState } from 'react';
 import { deleteBook } from '../../redux/books/bookSlice';
 import ProgressCircle from '../progressCircle/ProgressCircle';
+import styles from './bookStyle.module.scss';
 
 const Book = (props) => {
   const dispatch = useDispatch();
@@ -18,25 +19,39 @@ const Book = (props) => {
   };
 
   return (
-    <div>
-      <input onChange={changeHandler} type="text" name="value" placeholder="circle" />
-      <p>
-        {}
-      </p>
-      <p>
-        I am a book, my title is:
-        {title}
-      </p>
-      <p>
-        And my category is:
-        {' '}
-        {category}
-      </p>
-      <button onClick={handleRemove} type="button">Remove me</button>
-      <ProgressCircle
-        percent={percent}
-      />
-    </div>
+    <>
+      <input onChange={changeHandler} type="text" name="value" placeholder="test percent" />
+      <div className={styles.mainContainer}>
+        <div className={styles.dataContainer}>
+          <p className={styles.category}>
+            {category}
+          </p>
+          <p className={styles.title}>
+            {title}
+          </p>
+          <p className={styles.author}>
+            author
+          </p>
+          <div className={styles.optionsContainer}>
+            <button className={styles.btn} type="button">Comments</button>
+            <button className={styles.btn} onClick={handleRemove} type="button">Remove</button>
+            <button className={styles.btn} type="button">Edit</button>
+          </div>
+        </div>
+        <div className={styles.progressContainer}>
+          <div className={styles.circlePercent}>
+            <ProgressCircle
+              percent={percent}
+            />
+          </div>
+        </div>
+        <div className={styles.chapterContainer}>
+          <p className={styles.currentChapter}>CURRENT CHAPTER</p>
+          <p className={styles.chapterName}>A chapter</p>
+          <button className={styles.btn2} type="button">UPDATE PROGRESS</button>
+        </div>
+      </div>
+    </>
   );
 };
 export default Book;
